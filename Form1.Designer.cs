53e7ef7315203595a400c95b11d7d557c9d37937
samuelvictorol <samuel.softdev@outlook.com>
2022-05-17 02:19:43 -0300

    Subido

---
+++ Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace WFSqlCsEx1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txt_idade = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_excluir = new System.Windows.Forms.PictureBox();
+            this.btn_inserir = new System.Windows.Forms.PictureBox();
+            this.btn_atualizar = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_excluir)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_inserir)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_atualizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(174, 396);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nome:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.txt_nome.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_nome.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nome.ForeColor = System.Drawing.Color.Turquoise;
+            this.txt_nome.Location = new System.Drawing.Point(3, 9);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(246, 20);
+            this.txt_nome.TabIndex = 4;
+            this.txt_nome.Click += new System.EventHandler(this.txt_nome_Click);
+            this.txt_nome.TextChanged += new System.EventHandler(this.txt_nome_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.GhostWhite;
+            this.panel1.ForeColor = System.Drawing.Color.Ivory;
+            this.panel1.Location = new System.Drawing.Point(3, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(249, 1);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txt_nome);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Location = new System.Drawing.Point(283, 396);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(252, 32);
+            this.panel2.TabIndex = 6;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txt_idade);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.ForeColor = System.Drawing.Color.Turquoise;
+            this.panel3.Location = new System.Drawing.Point(283, 459);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(252, 32);
+            this.panel3.TabIndex = 8;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // txt_idade
+            // 
+            this.txt_idade.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.txt_idade.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_idade.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_idade.ForeColor = System.Drawing.Color.Aquamarine;
+            this.txt_idade.Location = new System.Drawing.Point(3, 9);
+            this.txt_idade.Name = "txt_idade";
+            this.txt_idade.Size = new System.Drawing.Size(246, 20);
+            this.txt_idade.TabIndex = 4;
+            this.txt_idade.Click += new System.EventHandler(this.txt_idade_Click);
+            this.txt_idade.TextChanged += new System.EventHandler(this.txt_idade_TextChanged);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.GhostWhite;
+            this.panel4.ForeColor = System.Drawing.Color.Ivory;
+            this.panel4.Location = new System.Drawing.Point(3, 31);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(249, 1);
+            this.panel4.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(174, 459);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 32);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Idade: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.txt_email);
+            this.panel5.Controls.Add(this.panel6);
+            this.panel5.Location = new System.Drawing.Point(283, 517);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(252, 32);
+            this.panel5.TabIndex = 10;
+            // 
+            // txt_email
+            // 
+            this.txt_email.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.txt_email.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_email.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_email.ForeColor = System.Drawing.Color.Turquoise;
+            this.txt_email.Location = new System.Drawing.Point(3, 9);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(249, 20);
+            this.txt_email.TabIndex = 4;
+            this.txt_email.Click += new System.EventHandler(this.txt_email_Click);
+            this.txt_email.TextChanged += new System.EventHandler(this.txt_email_TextChanged);
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.GhostWhite;
+            this.panel6.ForeColor = System.Drawing.Color.Ivory;
+            this.panel6.Location = new System.Drawing.Point(3, 31);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(249, 1);
+            this.panel6.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(174, 517);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 32);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Email";
+            // 
+            // btn_excluir
+            // 
+            this.btn_excluir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_excluir.Image = ((System.Drawing.Image)(resources.GetObject("btn_excluir.Image")));
+            this.btn_excluir.Location = new System.Drawing.Point(474, 601);
+            this.btn_excluir.Name = "btn_excluir";
+            this.btn_excluir.Size = new System.Drawing.Size(61, 50);
+            this.btn_excluir.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btn_excluir.TabIndex = 12;
+            this.btn_excluir.TabStop = false;
+            this.btn_excluir.Click += new System.EventHandler(this.btn_excluir_Click);
+            // 
+            // btn_inserir
+            // 
+            this.btn_inserir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_inserir.Image = ((System.Drawing.Image)(resources.GetObject("btn_inserir.Image")));
+            this.btn_inserir.Location = new System.Drawing.Point(180, 601);
+            this.btn_inserir.Name = "btn_inserir";
+            this.btn_inserir.Size = new System.Drawing.Size(61, 50);
+            this.btn_inserir.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btn_inserir.TabIndex = 13;
+            this.btn_inserir.TabStop = false;
+            this.btn_inserir.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // btn_atualizar
+            // 
+            this.btn_atualizar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_atualizar.Enabled = false;
+            this.btn_atualizar.Image = ((System.Drawing.Image)(resources.GetObject("btn_atualizar.Image")));
+            this.btn_atualizar.Location = new System.Drawing.Point(330, 601);
+            this.btn_atualizar.Name = "btn_atualizar";
+            this.btn_atualizar.Size = new System.Drawing.Size(61, 50);
+            this.btn_atualizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btn_atualizar.TabIndex = 14;
+            this.btn_atualizar.TabStop = false;
+            this.btn_atualizar.Click += new System.EventHandler(this.btn_atualizar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(666, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(59, 53);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(713, 284);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(737, 687);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_atualizar);
+            this.Controls.Add(this.btn_inserir);
+            this.Controls.Add(this.btn_excluir);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_excluir)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_inserir)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_atualizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox btn_excluir;
+        private System.Windows.Forms.PictureBox btn_inserir;
+        private System.Windows.Forms.PictureBox btn_atualizar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        public System.Windows.Forms.TextBox txt_nome;
+        public System.Windows.Forms.TextBox txt_idade;
+        public System.Windows.Forms.TextBox txt_email;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WFSqlCsEx1
+{
+    public partial class Form1 : Form
+    {
+        public string con = @"Data Source=LAPTOP-HOL6N2JR\SQLEXPRESS;Initial Catalog=dbCadastro;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Deseja Sair Realmente?", "Sair", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                return;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection objCon = new SqlConnection(con);
+            objCon.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * from CLIENTE", objCon);
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            da.SelectCommand = cmd;
+            da.Fill(ds);
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = ds.Tables[0].TableName;
+            objCon.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void txt_nome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_nome_Click(object sender, EventArgs e)
+        {
+            ClearLabPan();
+            label2.ForeColor = Color.Aquamarine;
+            panel1.BackColor = Color.Aquamarine;
+        }
+
+        private void ClearLabPan()
+        {
+            label1.ForeColor = Color.GhostWhite;
+            label2.ForeColor = Color.GhostWhite;
+            label3.ForeColor = Color.GhostWhite;
+            panel1.BackColor = Color.GhostWhite;
+            panel4.BackColor = Color.GhostWhite;
+            panel6.BackColor = Color.GhostWhite;
+        }
+
+        private void txt_idade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_email_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_idade_Click(object sender, EventArgs e)
+        {
+            ClearLabPan();
+            label1.ForeColor = Color.Aquamarine;
+            panel4.BackColor = Color.Aquamarine;
+        }
+        private void txt_email_Click(object sender, EventArgs e)
+        {
+            ClearLabPan();
+            label3.ForeColor = Color.Aquamarine;
+            panel6.BackColor = Color.Aquamarine;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dbCadastroDataSetBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection objCon = new SqlConnection(con);
+            if (txt_nome.Text == "" || txt_idade.Text == "" || txt_email.Text == "")
+            {
+                MessageBox.Show("Campo Não Preenchido!");
+                txt_nome.Focus();
+                LimpaCampos();
+                return;
+            }
+            try
+            {
+                int idade = int.Parse(txt_idade.Text);
+                string sql = $"INSERT INTO CLIENTE(NOME, IDADE, EMAIL) VALUES('{txt_nome.Text}', {idade}, '{txt_email.Text}')";
+
+                SqlCommand cmd = new SqlCommand(sql, objCon);
+                cmd.CommandType = CommandType.Text;
+                objCon.Open();
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    MessageBox.Show("Dado Cadastrado com Sucesso");
+                    LimpaCampos();
+                    Form1_Load(sender, e);
+                }
+            }
+            catch (Exception ex)
+            {
+                LimpaCampos();
+                MessageBox.Show("Dado Nao Cadastrado!\n" + ex.Message);
+            }
+            finally
+            {
+                objCon.Close();
+            }
+        }
+
+        public void LimpaCampos()
+        {
+            txt_nome.Focus();
+            txt_nome.Clear();
+            txt_email.Clear();
+            txt_idade.Clear();
+
+        }
+
+        private void btn_excluir_Click(object sender, EventArgs e)
+        {
+            string sql = $"DELETE FROM CLIENTE WHERE NOME = '{txt_nome.Text}'";
+            SqlConnection objConn= new SqlConnection(con);
+            
+            SqlCommand cmd = new SqlCommand(sql, objConn);
+
+            cmd.CommandType = CommandType.Text;
+            objConn.Open();
+
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                    {
+                    MessageBox.Show("Registro Excluido");
+                    LimpaCampos();
+                    Form1_Load(sender, e);
+                    }
+            }catch (Exception ex)
+            {
+                LimpaCampos();
+                MessageBox.Show("Nao foi possivel apagar o registro");
+                throw;
+            }
+            finally
+            {
+                objConn.Close();
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txt_nome.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txt_idade.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txt_email.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void btn_atualizar_Click(object sender, EventArgs e)
+        {
+   
+        }
+    }
+}
